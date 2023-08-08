d1927d261e00cfede0c3ca4decb852ee37e3d1dd
Desmondflexy <ikilodesmond@gmail.com>
2023-08-08 03:29:00 +0100

    routes updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,11 @@
 import express from 'express';
-import * as controller from "../controllers/user"
 
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
   // render view from views/index.ejs
-  res.render('index', {
-    title: 'TOPIT RECHARGE APP',
-    value: 'check check 1, 2, 3, testing'
-  });
+  res.render('index', {});
 });
 
 /* GET login page. */
@@ -31,16 +27,11 @@ router.get('/signup', function (req, res) {
 
 /* GET contact page. */
 router.get('/contact', function (req, res) {
-  // render view from views/login.ejs
+  // render view from views/contact.ejs
   res.render('contact', {
     error: '',
   });
 });
-
-// admin and users can SIGNUP and LOGIN from these routes
-router.post('/admin/signup', controller.signup);
-router.post('/signup', controller.signup);
-router.post("/login", controller.login);
 
 // import router into app.ts
 export default router;
